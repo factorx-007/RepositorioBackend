a1033c96adf895efeb71b9f239c2a934bb3e960d
Franclin Gonzales--FactorX <franclin.gonzalez@tecsup.edu.pe>
2025-05-06 23:35:52 -0500

    Agregamos package.json

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const http = require('http')
+
+function requestController(){
+    console.log('Bienvenidos al curso ')
+}
+const server = http.createServer(requestController)
+const PORT = process.env.PORT 
+
+server.listen(PORT, function(){
+    console.log(`Servidor Corriendo en el : `+PORT)
+})
